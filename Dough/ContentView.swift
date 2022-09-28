da51d24858e53dfb0488d158c2e3bf36e3406ee9
mdukek1995 <89478873+mdukek1995@users.noreply.github.com>
2022-09-28 13:04:30 -0600

    Created baseline for home screen, account view, and some math functions

--- Dough/ContentView.swift
+++ Dough/ContentView.swift
@@ -7,15 +7,107 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Image("logo")
+            .resizable()
+            .frame(width: 100, height: 100)
+            .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+            
+            TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
+                Text("Financial Goal #1").tabItem { /*@START_MENU_TOKEN@*/Text("Goal #1")/*@END_MENU_TOKEN@*/ }.tag(1)
+                Text("Financial Goal #2").tabItem { /*@START_MENU_TOKEN@*/Text("Goal #2")/*@END_MENU_TOKEN@*/ }.tag(2)
+
+            }
+            Spacer()
+                .padding(.vertical, 0.0)
+            Text("You can spend")
+            Text("$732.89")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.vertical)
+            Text("(and still reach your goals)")
+                .padding(.bottom)
+            Spacer()
+            Image("add button")
+                .resizable()
+                .frame(width: 40.0, height: 40.0)
+
+        }
+    }
+    
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
     }
 }
+}
 
-struct ContentView_Previews: PreviewProvider {
+struct AccountCalculations {
+    //calculating financials -will need user input//
+    var expenses = 1700
+    var income = 2000
+    var goal = 250
+    
+    //find difference between income & expenses//
+    //func excess(){
+       // income-expenses;
+  //  }
+    
+    //determine spending//
+   // func spending(){
+      //  excess-goal;
+   // }
+}
+
+struct AccountView: View{
+    var body: some View {
+        VStack{
+            Image("logo")
+            .resizable()
+            .frame(width: 100, height: 100)
+            .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+            
+            Text("Expenses")
+            .bold()
+           
+            // Text("$" + AccountCalculations.init(expenses))
+            
+            Spacer()
+            
+            Text("Income")
+            .bold()
+          //  Text("$" + /(AccountCalculations.init(income)))
+            
+            Spacer()
+            
+            Text("Goals")
+                .bold()
+                .padding()
+            HStack{
+                VStack{
+                    Image(systemName: "airplane")
+                    Text("$300")
+                }
+                VStack{
+                    Image(systemName: "house")
+                    Text("$12,500")
+                       
+                }
+                .padding(.horizontal)
+                VStack {
+                    Image(systemName: "car")
+                    Text("$4,200")
+                }
+            }
+            Spacer()
+        }
+        
+ }
+}
+struct AccountView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        AccountView()
     }
 }
--- Dough/DoughApp.swift
+++ Dough/DoughApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct DoughApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView();
+            AccountView()
         }
     }
 }
